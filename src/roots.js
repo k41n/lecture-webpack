560f83f615ea3def394ff1a1fe8bc14eced8771d
Andrei Malyshev <Andrei.Malyshev@kodep.ru>
2017-03-08 10:50:41 +0300

    It works

---
+++ src/roots.js
@@ -0,0 +1,26 @@
+var RootsApp = {
+  find: function(expression) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+        document.getElementById("result").innerHTML = JSON.parse(xmlhttp.responseText).result;
+       }
+    }
+
+   xmlhttp.open("GET", "https://newton.now.sh/zeroes/" + encodeURIComponent(expression), true);
+   xmlhttp.send();
+  },
+
+  init: function() {
+    var form = document.getElementById('form')
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      var expression = event.target[0].value
+      RootsApp.find(expression)
+    })
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  RootsApp.init()
+})
